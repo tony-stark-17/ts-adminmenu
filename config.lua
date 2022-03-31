28c9a745cd0a51d7bd54b44675aed79f6f883e41
FRANK <89828815+FRANK-Infinity@users.noreply.github.com>
2022-03-31 13:30:22 +0530

    Create config.lua
    Created config

---
+++ config.lua
@@ -0,0 +1,9 @@
+TConfig = {}
+------------------------- Commands
+TConfig.OpenAdminmenu = 'tsadmin' -- Name of command to open admin menu
+TConfig.ToggleNoClipMode = 'noclip2' -- Name of command to ToggleNoClip
+TConfig.Announce = 'ann' -- Name of command to use Announce
+TConfig.DebugTable = 'tsgetmenu' -- Name of command to use DebugTable
+------------------------- Keys ---
+TConfig.OpenMenuDefultKey = 'f11' -- Name of key to open admin menu 
+TConfig.OpenMenuNoclipDefultKey = 'insert' -- Name of key to open ToggleNoClip
